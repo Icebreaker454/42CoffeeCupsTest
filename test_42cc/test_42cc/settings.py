869a581cf2c94bfb7efb23409b8670e4af86a974
Paul Pukach <pavlopukach@gmail.com>
2015-03-22 08:16:14 +0200

    Minor security changes

--- test_42cc/test_42cc/settings.py
+++ test_42cc/test_42cc/settings.py
@@ -10,14 +10,13 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from .key import SECRET_KEY
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(79)@qnl^2a#=!5ej36zt^&4cblng_m4qvkcby05@=x=7)q^ef'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
