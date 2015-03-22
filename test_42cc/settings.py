158fb75d1f29a52352641823a24aa3aaa48ccc37
Paul Pukach <pavlopukach@gmail.com>
2015-03-22 23:24:31 +0200

    ticket:1 yet another file reorganization

--- test_42cc/settings.py
+++ test_42cc/settings.py
@@ -8,12 +8,17 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+from .key import SECRET_KEY
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
+
 TEMPLATE_DEBUG = True
+
 ALLOWED_HOSTS = []
 # Application definition
 INSTALLED_APPS = (
