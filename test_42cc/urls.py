43f459bd63c1f48fc54cb1adba030e51bea0fe2b
Paul Pukach <pavlopukach@gmail.com>
2015-03-22 23:19:05 +0200

    Another file reorganization...

---
+++ test_42cc/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from ticket1.views import IndexView
+
+urlpatterns = patterns(
+    '',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', IndexView.as_view(), name='home')
+)
