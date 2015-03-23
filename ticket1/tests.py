c265246d434b3919fbf06e661694a1db613844a1
Paul Pukach <pavlopukach@gmail.com>
2015-03-23 19:24:20 +0200

    ticket:3 added functional tests for the requests page

--- ticket1/tests.py
+++ ticket1/tests.py
@@ -21,7 +21,7 @@ class Ticket1Test(TestCase):
 
     def test_auth(self):
         user = User.objects.first()
-        self.client.login()
+        self.client.login(user.username, user.password)
 
     def test_page_info(self):
         """ Test whether the page displays data """
@@ -38,3 +38,20 @@ class Ticket1Test(TestCase):
         self.assertIn('Phone: +39912034', response.content)
 
 
+class Ticket2Test(TestCase):
+    """ The testing case for ticket 2 """
+    def test_requests_page_presence(self):
+        response = self.client.get('/requests')
+        self.assertEqual(response.status_code, 200)
+
+    def test_requests_page_content(self):
+
+        response = self.client.get('/requests')
+        try:
+            self.assertContains('http://', response.content)
+            self.assertContains('/requests', response.content)
+            self.assertContains(response.status_code, response.content)
+            self.assertContains('GET', response.content)
+            self.assertContains('(1)', response.title)
+        except AttributeError:
+            self.fail("Test page not implemented yet")
